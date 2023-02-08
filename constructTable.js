b0d1b33495ba94ee6101bab585b803c25c43e061
Andrew Chen <anew318@hotmail.com>
2023-02-08 12:05:48 +0800

    Test Change again

--- constructTable.js
+++ constructTable.js
@@ -2,18 +2,17 @@ fetch("Data/Deities/MasterList.csv")
   .then(function (response){return response.text();})
   .then(function (data){
 	  const jsons = data.split('\n');
+	  const table = document.getElementById("DeityTable");
 	  
 	  for (let json of jsons) {
 		fetch("Data/Deities/" + json)
 		  .then(function (response){return response.json();})
 		  .then(function (data){
-			let table = document.getElementById("DeityTable");
-			 
 			const list = JSON.parse(JSON.stringify(data));
-						 
-			var cols = [];
+			
+			let cols = [];
 			for (let i = 0; i < list.length; i++) {
-				for (var k in list[i]) {
+				for (let k in list[i]) {
 					if (cols.indexOf(k) === -1) { //Prevents duplication somehow
 						cols.push(k);
 					}
@@ -22,8 +21,8 @@ fetch("Data/Deities/MasterList.csv")
 			
 			for (let i = 0; i < list.length; i++) { // Adding the data to the table
 				trow = table.insertRow(-1); // Create a new row
-				for (var j = 0; j < cols.length; j++) {
-					var cell = trow.insertCell(-1);
+				for (let j = 0; j < cols.length; j++) {
+					let cell = trow.insertCell(-1);
 					cell.innerHTML = list[i][cols[j]]; // Inserting the cell at particular place
 				}
 			}
