08e1a47306fddb5c0fa8dd265c3d10b61847d53d
Andrew Chen <anew318@hotmail.com>
2023-02-08 10:05:07 +0800

    Time to test

--- constructTable.js
+++ constructTable.js
@@ -1,31 +1,37 @@
-fetch("Deities.json")
+fetch("Data/Deities/MasterList.csv")
   .then(function (response){return response.json();})
   .then(function (data){
-	var table = document.getElementById("DeityTable");
-	 
-	const list = JSON.parse(JSON.stringify(data));
-	             
-	var cols = [];
-	for (var i = 0; i < list.length; i++) {
-		for (var k in list[i]) {
-			if (cols.indexOf(k) === -1) { //Prevents duplication somehow
-				cols.push(k);
-			}
-		}
-	}
-	
-	// Adding the data to the table
-	for (var i = 0; i < list.length; i++) {
-		// Create a new row
-		trow = table.insertRow(-1);
-		if (i % 2 != 0) trow.style.background = "#626262";
-		for (var j = 0; j < cols.length; j++) {
-			var cell = trow.insertCell(-1);
+	  const jsons = data.text().split(",");
+	  
+	  for (let json in jsons) {
+		fetch(json)
+		  .then(function (response){return response.json();})
+		  .then(function (data){
+			let table = document.createElement("table");
 			 
-			// Inserting the cell at particular place
-			cell.innerHTML = list[i][cols[j]];
-		}
-	}
-	
-	sortTable(1);
-  })
+			const list = JSON.parse(JSON.stringify(data));
+						 
+			var cols = [];
+			for (let i = 0; i < list.length; i++) {
+				for (var k in list[i]) {
+					if (cols.indexOf(k) === -1) { //Prevents duplication somehow
+						cols.push(k);
+					}
+				}
+			}
+			
+			for (let i = 0; i < list.length; i++) { // Adding the data to the table
+				trow = table.insertRow(-1); // Create a new row
+				for (var j = 0; j < cols.length; j++) {
+					var cell = trow.insertCell(-1);
+					cell.innerHTML = list[i][cols[j]]; // Inserting the cell at particular place
+				}
+			}
+			
+			let MainTable = document.getElementById("DeityTable");
+			MainTable.appendChild(table);
+		  });
+	  }
+	  sortTable(1);
+  });
+
